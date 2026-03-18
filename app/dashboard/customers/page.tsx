bb69cce06cf6097ace6d1aedc2077ffcdd466e4b
Daniel <daniel.asenov@incentro.com>
2026-03-18 12:46:37 +0100

    Improved copying UI

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,6 @@
 import postgres from "postgres";
 import CustomerList from "@/app/ui/customers/list";
+import { lusitana } from "@/app/ui/fonts";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -10,6 +11,9 @@ async function getAllCustomers() {
 export default async function Page() {
     const customers = await getAllCustomers();
     return (
-        <CustomerList customers={customers} />
+        <>
+            <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
+            <CustomerList customers={customers} />
+        </>
     );
 }
--- app/ui/customers/list.tsx
+++ app/ui/customers/list.tsx
@@ -1,31 +1,47 @@
 "use client";
 
 import Image from "next/image";
-import CustomerEmail from "./customer-email";
 import { Row, RowList } from "postgres";
 import { useState } from "react";
+import clsx from "clsx";
+import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 
 export default function CustomerList({ customers }: { customers: RowList<Row[]> }) {
     const [copied, setCopied] = useState("");
 
     function copyToClipboard(text: string) {
+        if (text === copied) return;
         try {
             navigator.clipboard.writeText(text);
             setCopied(text);
         } catch (exception) { console.error("Failed to copy to clipboard"); }
     }
 
-    return (<div className="flex flex-col divide-y-2 divide-gray-200">
-        {customers?.map((customer) => (
-            <div key={customer.id} className="flex flex-row items-center gap-4 h-12">
-                <Image
-                    src={customer.image_url} width={28} height={28} alt={customer.name}
-                    className="rounded-full select-none"
-                />
-                <div>{customer.name}</div>
-
-                <CustomerEmail email={customer.email} copied={copied} onCopy={() => copyToClipboard(customer.email)} />
+    return (
+        <>
+            {copied && <div className="flex flex-row items-center gap-1 py-2 text-gray-500">
+                <span>Copied "{copied}"</span>
+                <CheckBadgeIcon className="w-6" />
+            </div>}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 select-none">
+                {customers?.map((customer) => (
+                    <div key={customer.id} className="flex flex-col rounded-md bg-gray-200 overflow-hidden gap-2 relative">
+                        <div className="grid grid-cols-[20px_auto] justify-center items-center gap-2 p-2 mb-8">
+                            <Image
+                                src={customer.image_url} width={28} height={28} alt={customer.name}
+                                className="rounded-full"
+                            />
+                            <div className="font-bold text-l flex justify-center">{customer.name}</div>
+                        </div>
+                        <button className={clsx(
+                            "flex justify-center text-white bg-blue-400 py-1 transition duration-100 hover:brightness-90 w-full absolute bottom-0",
+                            {
+                                "bg-green-400 text-green-900": copied === customer.email
+                            }
+                        )} onClick={() => copyToClipboard(customer.email)}>{customer.email}</button>
+                    </div>
+                ))}
             </div>
-        ))}
-    </div>)
+        </>
+    )
 }
